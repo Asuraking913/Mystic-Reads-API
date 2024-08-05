de530317b3e04d77dfeef298f6ceb35f9fc64185
Israel Shedrack <israelshedrack913@gmail.com>
2024-08-05 22:35:19 +0100

    reconfigured app.py

--- run.py
+++ run.py
@@ -2,8 +2,8 @@ from app import create_app
 from os import system
 from events import socket
 
-flask_app = create_app()
+app = create_app()
 
 if __name__ == '__main__':
-    flask_app.run(debug=True)
+    app.run(debug=True)
     # socket.run(flask_app, debug=True)
