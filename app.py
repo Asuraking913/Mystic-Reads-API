b808bb8ecd0a1327cfe4636684fbdcfb8beabdb6
Israel Shedrack <israelshedrack913@gmail.com>
2024-08-05 22:33:04 +0100

    new changes to vercel.json

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ from config import AppConfig
 from routes import root_routes
 from extensions import db, jwt
 from flask_cors import CORS
-from events import socket, root_socket
+# from events import socket, root_socket
 
 def create_app():
     
@@ -12,17 +12,17 @@ def create_app():
     app.config.from_object(AppConfig)
     db.init_app(app)
     jwt.init_app(app)
-    socket.init_app(app, cors_allowed_origins = 'http://localhost:5173')
+    # socket.init_app(app, cors_allowed_origins = 'http://localhost:5173')
     # cors = CORS(app, resources={r"/*": {"origins": "*"}})
     CORS(app, resources={r'/*': {"origins" : 'http://localhost:5173'}})
     root_routes(app, db)
-    root_socket(socket, db)
+    # root_socket(socket, db)
 
     with app.app_context():
         db.create_all()
 
     return app
 
-if __name__ == "__main__":
-    flask_app = create_app()
-    flask_app.run()
+# if __name__ == "__main__":
+#     flask_app = create_app()
+#     flask_app.run()
--- run.py
+++ run.py
@@ -5,5 +5,5 @@ from events import socket
 flask_app = create_app()
 
 if __name__ == '__main__':
-    # flask_app.run(debug=True)
-    socket.run(flask_app, debug=True)
+    flask_app.run(debug=True)
+    # socket.run(flask_app, debug=True)
