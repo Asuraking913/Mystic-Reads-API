1943ce72f1f716145e63532e1ca4614f5f5e2f78
Israel Shedrack <israelshedrack913@gmail.com>
2024-08-10 16:06:53 +0100

    new changes to config.py

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ from config import AppConfig
 from routes import root_routes
 from extensions import db, jwt
 from flask_cors import CORS
-# from events import socket, root_socket
+from events import socket, root_socket
 
 def create_app():
     
@@ -12,10 +12,11 @@ def create_app():
     app.config.from_object(AppConfig)
     db.init_app(app)
     jwt.init_app(app)
-    # socket.init_app(app, cors_allowed_origins = 'http://localhost:5173')
+    socket.init_app(app, cors_allowed_origins = 'http://localhost:5173')
     # cors = CORS(app, resources={r"/*": {"origins": "*"}})
     CORS(app, supports_credentials= True, resources={r'/*': {"origins" : 'https://mystic-reads.vercel.app'}})
     root_routes(app, db)
+    # socket.init_app(app)
     # root_socket(socket, db)
 
     with app.app_context():
--- run.py
+++ run.py
@@ -5,6 +5,6 @@ from events import socket
 app = create_app()
 
 if __name__ == '__main__':
-    app.run(debug=True)
-    # socket.run(flask_app, debug=True)
+    # app.run(debug=True)
+    socket.run(app, debug=True)
     
